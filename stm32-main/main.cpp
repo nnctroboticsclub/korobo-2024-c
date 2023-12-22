b9465f6ee02f573fd88333cc03335231b5b5c2a9
syoch <syoch64@gmail.com>
2023-12-22 21:42:06 +0000

    Update: Setup STM32-Main

---
+++ stm32-main/main.cpp
@@ -0,0 +1,50 @@
+#include "mbed.h"
+#include "rtos.h"
+
+using namespace std::chrono_literals;
+
+Ticker ticker;
+DigitalOut led1(LED1);
+CAN can(PB_8, PB_9, 1000000);
+
+char counter = 0;
+void ReceiveLoop() {
+  CANMessage msg;
+  while (1) {
+    if (can.read(msg)) {
+      printf("Message received ID=0x%#x: ", msg.id);
+      for (int i = 0; i < msg.len; i++) {
+        printf("%02x ", msg.data[i]);
+      }
+      printf("\n");
+    }
+
+    ThisThread::sleep_for(1ms);
+  }
+}
+void SendLoop() {
+  CANMessage msg(0x0, "A", 1, CANData, CANStandard);
+
+  while (1) {
+    auto r = can.write(msg);
+    if (r == 0) {
+      printf("failed to send message\n");
+    }
+    ThisThread::sleep_for(1000ms);
+  }
+}
+
+int main() {
+  printf("main()\n");
+
+  Thread t;
+  t.start(ReceiveLoop);
+
+  Thread t2;
+  t2.start(SendLoop);
+
+  while (1) {
+    led1 = !led1;
+    ThisThread::sleep_for(500ms);
+  }
+}
