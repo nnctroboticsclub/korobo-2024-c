b1708942262de5b8276f758349d6927b47807e7e
syoch <syoch64@gmail.com>
2024-01-01 06:31:36 +0000

    Update: Rewrite Distributed CAN

--- stm32-main/dcan.hpp
+++ stm32-main/dcan.hpp
@@ -3,39 +3,20 @@
 #include "mbed.h"
 #include "rtos.h"
 
-class DistributedCAN {
-  int can_id = 0x7;
+#include <unordered_map>
+#include <vector>
+
+class SimpleCAN {
+ public:
+  using RxCallback = std::function<void(SimpleCAN &, CANMessage const &)>;
+
+ private:
   CAN can_;
   int freqency_ = 50E3;
 
+  std::vector<RxCallback> rx_callbacks_;
+
   Thread thread_;
-
-  // 1 -> success
-  inline int WriteMessage(uint32_t id, std::vector<uint8_t> const &data) {
-    CANMessage msg;
-    msg.id = id;
-    msg.len = data.size();
-    std::copy(data.begin(), data.end(), msg.data);
-    return can_.write(msg);
-  }
-
-  inline void HandleMessage(CANMessage const &message) {
-    if (message.id == 0x80)  // global ping
-    {
-      WriteMessage(0x81 + can_id, {});
-    } else if (message.id == 0x40)  // Control packet
-    {
-      if (message.data[0] == 0x02) {
-        WriteMessage(0x82, {0x02});
-      }
-    } else {
-      printf("Message received from %#lx: ", message.id);
-      for (int i = 0; i < message.len; i++) {
-        printf("%02x ", message.data[i]);
-      }
-      printf("\n");
-    }
-  }
 
   void ThreadMain() {
     CANMessage msg;
@@ -46,19 +27,80 @@ class DistributedCAN {
       }
 
       if (can_.read(msg)) {
-        HandleMessage(msg);
+        for (auto const &cb : rx_callbacks_) {
+          cb(*this, msg);
+        }
       }
     }
   }
 
  public:
-  DistributedCAN(int can_id, PinName rx, PinName tx, int freqency = 50E3)
-      : can_id(can_id), can_(rx, tx, freqency), freqency_(freqency) {}
-
-  void Init() { thread_.start(callback(this, &DistributedCAN::ThreadMain)); }
-
   // 1 -> success
-  int Send(uint32_t id, std::vector<uint8_t> const &data) {
-    return WriteMessage(id, data);
+  inline int Send(uint32_t id, std::vector<uint8_t> const &data) {
+    CANMessage msg;
+    msg.id = id;
+    msg.len = data.size();
+    std::copy(data.begin(), data.end(), msg.data);
+    return can_.write(msg);
+  }
+
+  SimpleCAN(PinName rx, PinName tx, int freqency = 50E3)
+      : can_(rx, tx, freqency), freqency_(freqency) {}
+
+  void Init() { thread_.start(callback(this, &SimpleCAN::ThreadMain)); }
+
+  void OnRx(int priority, RxCallback cb) { rx_callbacks_.emplace_back(cb); }
+};
+
+class DistributedCAN {
+ public:
+  struct EventCallback {
+    uint8_t element_id;
+    std::function<void(std::vector<uint8_t>)> cb;
+  };
+
+ private:
+  int can_id = 0x7;
+  SimpleCAN can_;
+
+  std::vector<EventCallback> callbacks_;
+
+  inline void HandleMessage(CANMessage const &message) {
+    bool called = false;
+
+    for (auto const &cb : callbacks_) {
+      if (cb.element_id == message.id) {
+        cb.cb(std::vector<uint8_t>(message.data, message.data + message.len));
+        called = true;
+      }
+    }
+
+    if (!called) {
+      printf("Unhandled message: %d, Data: ", message.id);
+      for (int i = 0; i < message.len; i++) {
+        printf("%02X ", message.data[i]);
+      }
+      printf("\n");
+    }
+  }
+
+ public:
+  DistributedCAN(int can_id, PinName rx, PinName tx, int freqency = 50E3)
+      : can_id(can_id), can_(rx, tx, freqency) {}
+
+  void Init() {
+    can_.Init();
+    can_.OnRx(0, [this](SimpleCAN &can, CANMessage const &message) {
+      this->HandleMessage(message);
+    });
+
+    OnEvent(0x80, [this](std::vector<uint8_t> data) {
+      can_.Send(0x81 + can_id, {});
+    });
+  }
+
+  void OnEvent(uint8_t element_id,
+               std::function<void(std::vector<uint8_t>)> cb) {
+    callbacks_.emplace_back(EventCallback{element_id, cb});
   }
 };
