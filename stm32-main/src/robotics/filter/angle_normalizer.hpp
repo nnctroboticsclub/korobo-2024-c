50a2e3beeaf0ea5243714c95d74ff529f3a82f8d
syoch <syoch64@gmail.com>
2024-01-07 08:52:20 +0000

    Update: Ported angle normalizer from Simulation code

---
+++ stm32-main/src/robotics/filter/angle_normalizer.hpp
@@ -0,0 +1,20 @@
+template <typename T>
+class AngleNormalizer {
+  T prev_input = 0;
+  T value = 0;
+
+ public:
+  T Update(T input) {
+    T delta_rot_y_deg = input - prev_input;
+    prev_input = input;
+
+    if (delta_rot_y_deg > 180)
+      delta_rot_y_deg -= 360;
+    else if (delta_rot_y_deg < -180)
+      delta_rot_y_deg += 360;
+
+    value += delta_rot_y_deg;
+
+    return value;
+  }
+};
