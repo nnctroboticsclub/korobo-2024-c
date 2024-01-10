b5b81ef47b84b5bb6940b48d99fabcaaf479a15e
syoch <syoch64@gmail.com>
2024-01-10 13:21:27 +0000

    Update: Added BLDC output definition

---
+++ stm32-main/src/robotics/output/BLDC.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <mbed.h>
+#include "motor.hpp"
+
+namespace robotics::output {
+class BLDC : public Motor<float> {
+  int min_pulsewidth_, max_pulsewidth_;
+
+  PwmOut pwmout_;
+
+ public:
+  BLDC(PinName pin, int min_pulsewidth, int max_pulsewidth)
+      : pwmout_(pin),
+        min_pulsewidth_(min_pulsewidth),
+        max_pulsewidth_(max_pulsewidth) {
+    pwmout_.period_ms(20);
+    pwmout_.pulsewidth_us(0);
+  }
+
+  void SetSpeed(float speed) override {
+    if (speed < 0) {
+      // ESC ussualy doesn't support negative speed.
+      pwmout_.pulsewidth_us(min_pulsewidth_);
+    }
+
+    pwmout_.pulsewidth_us(min_pulsewidth_ +
+                          (max_pulsewidth_ - min_pulsewidth_) * speed);
+  }
+};
+}  // namespace robotics::output
