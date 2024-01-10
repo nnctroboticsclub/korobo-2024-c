1844191e5926f9cd44909d2bba93160f6abe71c4
syoch <syoch64@gmail.com>
2024-01-10 14:42:47 +0000

    Update: Added AngledMotorBuilder

--- stm32-main/src/robotics/fusion/angled_motor.hpp
+++ stm32-main/src/robotics/fusion/angled_motor.hpp
@@ -6,20 +6,42 @@
 
 namespace robotics::fusion {
 template <typename T>
+class AngledMotorConfig {
+  std::shared_ptr<sensor::encoder::Absolute<T>> encoder;
+  std::unique_ptr<output::Motor<T>> motor;
+};
+
+template <typename T>
+class AngledMotorConfigBuilder {
+  AngledMotorConfig<T> config;
+
+ public:
+  AngledMotorConfigBuilder& Encoder(
+      std::shared_ptr<sensor::encoder::Absolute<T>> encoder) {
+    config.encoder = std::move(encoder);
+    return *this;
+  }
+
+  AngledMotorConfigBuilder& Motor(std::unique_ptr<output::Motor<T>> motor) {
+    config.motor = std::move(motor);
+    return *this;
+  }
+
+  AngledMotorConfig<T> Build() { return std::move(config); }
+};
+
+template <typename T>
 class AngledMotor {
  public:
-  struct Config {
-    std::unique_ptr<sensor::encoder::Absolute<T>> encoder;
-    std::unique_ptr<output::Motor<T>> motor;
-  };
-
   filter::PID<float> angle_;
+
+ private:
   float target_angle_;
-  std::unique_ptr<sensor::encoder::Absolute<T>> encoder_;
+  std::shared_ptr<sensor::encoder::Absolute<T>> encoder_;
   std::unique_ptr<output::Motor<T>> motor_;
 
  public:
-  AngledMotor(Config&& config) {
+  AngledMotor(AngledMotorConfig<T>&& config) {
     encoder_ = std::move(config.encoder);
     motor_ = std::move(config.motor);
   }
