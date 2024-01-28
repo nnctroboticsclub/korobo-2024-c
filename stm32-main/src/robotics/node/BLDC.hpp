b07ce37ab321b61c465fe64fbbf94ed2d738fb31
syoch <syoch64@gmail.com>
2024-01-28 03:35:52 +0000

    Update: separated bldc initialization phase from 1 to 2

--- stm32-main/src/robotics/node/BLDC.hpp
+++ stm32-main/src/robotics/node/BLDC.hpp
@@ -5,11 +5,15 @@
 
 namespace robotics::node {
 class BLDC : public Motor<float> {
-  bool initialized = false;
+  enum Status { Initialized, ESCInit0, Ready };
+
   PwmOut pwmout_;
   int min_pulsewidth_, max_pulsewidth_;
+  Status status;
 
   void SetSpeed(float speed) override {
+    if (status != Status::Ready) return;
+
     if (speed < 0) {
       // ESC ussualy doesn't support negative speed.
       pwmout_.pulsewidth_us(min_pulsewidth_);
@@ -23,20 +27,23 @@ class BLDC : public Motor<float> {
   BLDC(PinName pin, int min_pulsewidth, int max_pulsewidth)
       : pwmout_(pin),
         min_pulsewidth_(min_pulsewidth),
-        max_pulsewidth_(max_pulsewidth) {
+        max_pulsewidth_(max_pulsewidth),
+        status(Status::Initialized) {
     pwmout_.period_us(2000);
     pwmout_.pulsewidth_us(0);
   }
 
-  void Init() {
-    if (initialized) return;
-    initialized = true;
+  void Init0() {
+    if (status != Status::Initialized) return;
 
     pwmout_.pulsewidth_us(max_pulsewidth_);
-    ThisThread::sleep_for(1s);
+    status = Status::ESCInit0;
+  }
+  void Init1() {
+    if (status != Status::ESCInit0) return;
 
     pwmout_.pulsewidth_us(min_pulsewidth_);
-    ThisThread::sleep_for(1s);
+    status = Status::Ready;
   }
 };
 }  // namespace robotics::node
