13e61fbb40828a213f15178f132c91450a25d779
syoch <syoch64@gmail.com>
2024-02-28 17:17:02 +0000

    Update: improved ws-relay

--- ws-relay/main.py
+++ ws-relay/main.py
@@ -1,8 +1,6 @@
 import time
 from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-import socket
-import multiprocessing
 
 app = FastAPI()
 
@@ -10,41 +8,6 @@ connected_clients: List[WebSocket] = []
 server: WebSocket | None = None
 c2s_pps = 0
 s2c_pps = 0
-
-
-class IPReporter:
-    def __init__(self):
-        pass
-
-    def send_ip(self):
-        lst = IPReporter.get_ip_address_list()
-        payload = IPReporter.pack_ip_address_list(lst)
-
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            # sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-            sock.sendto(payload, ("255.255.255.255", 38000))
-
-    @staticmethod
-    def pack_ip_address_list(lst: List[str]):
-        payload = b""
-        payload += len(lst).to_bytes(4, "big")
-        for ip in lst:
-            payload += len(ip).to_bytes(4, "big")
-            payload += ip.encode()
-
-        return payload
-
-    @staticmethod
-    def get_ip_address_list():
-        ip_list = [
-            ip[0]
-            for (fa, _, _, _, ip) in socket.getaddrinfo(socket.gethostname(), 0)
-            if fa == socket.AddressFamily.AF_INET
-        ]
-
-        return ip_list
 
 
 @app.get("/status")
@@ -66,7 +29,9 @@ async def client_handler(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_bytes()
-            # print("C->S", data.hex())
+            print(
+                f"{time.strftime('%H:%M:%S', time.localtime())} C->S: {len(data)}bytes"
+            )
             if server:
                 c2s_pps += 1
                 await server.send_bytes(data)
@@ -84,7 +49,9 @@ async def server_handler(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_bytes()
-            # print("S->C", data.hex())
+            print(
+                f"{time.strftime('%H:%M:%S', time.localtime())} S->C: {len(data)}bytes"
+            )
             s2c_pps += 1
             for client in connected_clients:
                 await client.send_bytes(data)
@@ -93,22 +60,7 @@ async def server_handler(websocket: WebSocket):
         server = None
 
 
-def reporter_process():
-    print("IP reporter process ended!")
-    app = IPReporter()
-    while 1:
-        time.sleep(1)
-        if server:
-            continue
-
-        app.send_ip()
-    print("IP reporter process ended!")
-
-
 if __name__ == "__main__":
-    proc = multiprocessing.Process(target=reporter_process)
-    proc.start()
-
     import uvicorn
 
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ ws-relay/reporter.py
@@ -0,0 +1,52 @@
+import asyncio
+import socket
+from typing import List
+
+
+class IPReporter:
+    def __init__(self):
+        pass
+
+    def send_ip(self):
+        lst = IPReporter.get_ip_address_list()
+        payload = IPReporter.pack_ip_address_list(lst)
+        print("Sending ips:", lst)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+            sock.sendto(payload, ("255.255.255.255", 38000))
+
+    @staticmethod
+    def pack_ip_address_list(lst: List[str]):
+        payload = b""
+        payload += len(lst).to_bytes(4, "big")
+        for ip in lst:
+            payload += len(ip).to_bytes(4, "big")
+            payload += ip.encode()
+
+        return payload
+
+    @staticmethod
+    def get_ip_address_list():
+        ip_list = [
+            ip[0]
+            for (fa, _, _, _, ip) in socket.getaddrinfo(socket.gethostname(), 0)
+            if fa == socket.AddressFamily.AF_INET
+        ]
+
+        return ip_list
+
+
+async def reporter():
+    print("IP reporter process started!")
+    app = IPReporter()
+    while 1:
+        await asyncio.sleep(1)
+        app.send_ip()
+    print("IP reporter process ended!")
+
+
+if __name__ == "__main__":
+    asyncio.run(reporter())
