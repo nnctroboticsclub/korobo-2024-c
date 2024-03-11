17417c8dc75346f78d56c5d15acd83156cee2a98
syoch <syoch64@gmail.com>
2024-03-11 05:15:57 +0000

    some

--- stm32-main/src/robotics/filter/angled_motor.hpp
+++ stm32-main/src/robotics/filter/angled_motor.hpp
@@ -3,20 +3,29 @@
 #include "pid.hpp"
 #include "../node/node.hpp"
 
+#include "./angle_smoother.hpp"
+
 namespace robotics::filter {
 template <typename T>
 class AngledMotor {
  public:
-  filter::PID<float> pid{1.0f / 36, 0.0f, 0.0f, 0.0f};
+  filter::PID<float> pid{1.0f / 360, 3.3f, 0.0f, 0.0f};
 
   Node<float> feedback;
   Node<float> goal;
   Node<float> output;
 
+ private:
+  AngleNormalizer<float> feedback_normalizer;
+  AngleNormalizer<float> goal_normalizer;
+
  public:
   AngledMotor() : feedback(0), goal(0), output(0) {
-    feedback.Link(pid.fb_);
-    goal.Link(pid.goal_);
+    feedback.Link(feedback_normalizer.input);
+    feedback_normalizer.output.Link(pid.fb_);
+
+    goal.Link(goal_normalizer.input);
+    goal_normalizer.output.Link(pid.goal_);
 
     pid.output_.Link(output);
   }
