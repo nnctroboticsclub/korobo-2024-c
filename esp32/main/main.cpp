fba09b014c06c2aba3e87bad2ce99ba1aa2b1685
syoch <syoch64@gmail.com>
2024-04-12 15:07:23 +0000

    Update: using Color::FromHSV instead of hsv2color

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -13,41 +13,6 @@
 
 using Color = robotics::utils::Color;
 
-Color hsv2color(float h, float s, float v) {
-  float c = v * s;
-  float x = c * (1 - std::abs(fmod(h / 60, 2) - 1));
-  float m = v - c;
-
-  float r, g, b;
-  if (h < 60) {
-    r = c;
-    g = x;
-    b = 0;
-  } else if (h < 120) {
-    r = x;
-    g = c;
-    b = 0;
-  } else if (h < 180) {
-    r = 0;
-    g = c;
-    b = x;
-  } else if (h < 240) {
-    r = 0;
-    g = x;
-    b = c;
-  } else if (h < 300) {
-    r = x;
-    g = 0;
-    b = c;
-  } else {
-    r = c;
-    g = 0;
-    b = x;
-  }
-
-  return Color((r + m) * 255, (g + m) * 255, (b + m) * 255);
-}
-
 extern "C" void app_main(void) {
   App app;
   app.Init();
